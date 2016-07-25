c6f9231393c55fdd01ec970224e3fe75410e2508
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-07-25 16:31:15 -0400

    added the like view to the home page

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -7,6 +7,7 @@ import angular from 'angular';
 function MainController( $scope,$http, SERVER  ){
 
 	$scope.cow =[];
+	$scope.post;
 
     $scope.test = _.range(20);
     console.log(_.chunk($scope.test, 2));
@@ -26,10 +27,76 @@ function MainController( $scope,$http, SERVER  ){
 
 		// 	console.log(res);
 		// })
-	    
+	      
+
+		$http.get(SERVER.URL).then((res)=>{
+
+	            
+			 	console.log(res);
+			 	console.log($stateParams.id)
+			 	$scope.post = res.data;
+                let blu = res.data
+			 	if ($scope.post.likes ===undefined ){
+            	$scope.post.likes = 0;
+                blu.likes = 0 ;
+                $('.many_likes').html(`${$scope.post.likes} Likes`)
+                console.log('hus');
+                // $scope.addLike();
+            }   else if($scope.post.likes === 1){
+             	$('.many_likes').html(`${post.likes} Like`)
+            }   else if($scope.post.likes > 1 ){
+            	$('.many_likes').html(`${$scope.post.likes} Likes`)
+            }
+			
+		});
+			// $http.delete(SERVER.URL + "5796021ee4f8050003543c6f").then((res)=>{
+
+			// console.log(res);
+		// })
+
+	
 
 
 	}
+
+
+      
+    // $scope.addLike = (post)=>{
+    //   if(post._id == post._id ){
+
+
+
+    // 	if (post.likes ===undefined ){
+    //         	post.likes = 0;
+             
+             
+    //         }  
+
+
+    //    post.likes += 1;
+
+    //     console.log('hfhfh')
+    //      	$http.put(SERVER.URL + post._id,post).then((res)=>{
+
+    //      		console.log(res)            
+    //             $http.get(SERVER.URL + post._id).then((res)=>{
+    //             console.log(res)
+    //             if(res.data.likes === 1){
+    //             $('.many_likes').html(`${res.data.likes} Like`)
+    //         }   else if(res.data.likes > 1 ){
+    //             $('.many_likes').html(`${res.data.likes} Likes`)
+    //         }
+            
+                  
+    //         });
+
+    //         });
+            
+    //  		};
+
+
+    //  	};
+
 
 	console.log($scope.cow)
 
